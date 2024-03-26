6f9691fbd6aad5f63c24812e4e9a6e8c8a71e650
Th0raxC0n <93955003+Th0raxC0n@users.noreply.github.com>
2024-03-26 01:26:26 -0400

    Update tiktok-live.service.ts

--- src/tiktok-live/tiktok-live.service.ts
+++ src/tiktok-live/tiktok-live.service.ts
@@ -5,7 +5,7 @@ import { EventsGateway } from 'src/events/events.gateway';
 @Injectable()
 export class TiktokLiveService {
 
-  private tiktokUsername = 'imsundry'//'randiilyn';
+  private tiktokUsername = 'randiilyn'//'randiilyn';
   private availableGifts: any[] = [];
   
   constructor(private eventsGateway: EventsGateway) {}
