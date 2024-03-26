37faaae11d6a2046d40313b953327c30646d16ac
Th0raxC0n <93955003+Th0raxC0n@users.noreply.github.com>
2024-03-26 01:12:15 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,8 @@
-// Other imports remain untouched
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as fs from 'fs';
-import * as https from 'https';
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync('/etc/letsencrypt/live/letsgo2.work/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/letsgo2.work/fullchain.pem'),
-  };
-  
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions,
-  });
-  
+  const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
-
 bootstrap();
