ae2b8708e99cd8d8d820ff4e16e6671a85c98429
Th0raxC0n <93955003+Th0raxC0n@users.noreply.github.com>
2024-05-12 12:44:27 -0400

    vv

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,13 @@ import * as fs from 'fs';
 import * as https from 'https';
 
 async function bootstrap() {
-  // const httpsOptions = {
-  //   key: fs.readFileSync('/etc/letsencrypt/live/letsgo2.work/privkey.pem'),
-  //   cert: fs.readFileSync('/etc/letsencrypt/live/letsgo2.work/fullchain.pem'),
-  // };
+  const httpsOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/letsgo2.work/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/letsgo2.work/fullchain.pem'),
+  };
   
   const app = await NestFactory.create(AppModule, {
-    // httpsOptions,
+    httpsOptions,
   });
   
   await app.listen(3005);
